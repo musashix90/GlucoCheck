38d41e44cc196b3fb83ce1617f2f75cf7164f3ad
Michael Tanner <mwtanner@my.waketech.edu>
2021-02-19 14:22:02 -0500

    Added SQLite migration configuration class

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace GlucoCheck.Migrations
+{
+    using System;
+    using System.Linq;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Data.SQLite.EF6.Migrations;
+    using GlucoCheck.Classes;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GlucoCheck.Classes.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("System.Data.SQLite", new SQLiteMigrationSqlGenerator());
+        }
+
+        protected override void Seed(AppDbContext context)
+        {
+            
+        }
+    }
+}
