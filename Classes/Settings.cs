3d473ea4c8d6b8698c1d2b10f18b79f783f071f9
mmswanson1 <71193277+mmswanson1@users.noreply.github.com>
2021-02-25 20:23:39 -0500

    Settings defaults added

--- Classes/Settings.cs
+++ Classes/Settings.cs
@@ -6,45 +6,33 @@ namespace GlucoCheck.Classes
     {
         #region Properties
 
-        // TODO: Specific thresholds needed? (i.e., fasting, before meal, after meal, before bed, etc...)
+        public double HighBSLThreshold { get; set; } = 200.0;
 
-        public float HighBSLThreshold { get; set; }
-
-        public float LowBSLThreshold { get; set; }
+        public double LowBSLThreshold { get; set; } = 70.0;
 
         // Maximum number of hours user will allow to pass since last entry before receiving reminder
         // Default maximum will be 24 hours to enter at least once per day
-        public int MaxDailyReminder { get; set; }
+        public int MaxDailyReminder { get; set; } = 24;
+
+        // Number of hours allowed to pass after "first" entry before receiving a reminder to enter a "second" entry.
+        // TODO: still waiting for group feedback about distinguishing entries based on when they were entered
+        // to change this setting to a "post-meal" reminder.
+        public double SecondEntryReminder { get; set; } = 2.0;
 
         // A property to determine if user wants measurements in mmol/L or mg/dL
         // Default set to False since US standard is mg/dL
-        public Boolean IsMillimoles { get; set; }
+        public Boolean IsMillimoles { get; set; } = false;
 
-        public DateTime ActiveStart { get; set; }
+        // ActiveStart and ActiveEnd defaults to null so "active hours" will be 24/7
+        public DateTime? ActiveStart { get; set; }
 
-        public DateTime ActiveEnd { get; set; }
+        public DateTime? ActiveEnd { get; set; }
 
         #endregion
 
         #region Constructors
 
-        // TODO: Complete no arg constructor speak w/ group about default value preferences - in constructor or in methods?
-        // Could provide default values for reminders, unit of measurement, and normal BSL thresholds
-        public Settings()
-        {
-
-        }
-
-        public Settings(float highBSLThreshold, float lowBSLThreshold, int maxDailyReminder,
-            Boolean isMillimoles, DateTime activeStart, DateTime activeEnd)
-        {
-            HighBSLThreshold = highBSLThreshold;
-            LowBSLThreshold = lowBSLThreshold;
-            MaxDailyReminder = maxDailyReminder;
-            IsMillimoles = isMillimoles;
-            ActiveStart = activeStart;
-            ActiveEnd = activeEnd;
-        }
+        public Settings() { }
 
         #endregion
 
