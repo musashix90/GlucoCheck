46a1d4d158354b4d5de1cd79435d294a3d455fee
Michael Tanner <mwtanner@my.waketech.edu>
2021-03-04 17:18:49 -0500

    Expanded the chart area, and added code to query the database for entries to graph.

--- Forms/FrmLogGraph.Designer.cs
+++ Forms/FrmLogGraph.Designer.cs
@@ -32,23 +32,24 @@ namespace GlucoCheck.Forms
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            this.bslchart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.bslchart)).BeginInit();
+            this.BSLChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.BSLChart)).BeginInit();
             this.SuspendLayout();
             // 
-            // bslchart
+            // BSLChart
             // 
             chartArea1.Name = "ChartArea1";
-            this.bslchart.ChartAreas.Add(chartArea1);
-            this.bslchart.Location = new System.Drawing.Point(12, 23);
-            this.bslchart.Name = "bslchart";
+            this.BSLChart.ChartAreas.Add(chartArea1);
+            this.BSLChart.Location = new System.Drawing.Point(18, 36);
+            this.BSLChart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BSLChart.Name = "BSLChart";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Name = "BSL";
-            this.bslchart.Series.Add(series1);
-            this.bslchart.Size = new System.Drawing.Size(550, 415);
-            this.bslchart.TabIndex = 0;
-            this.bslchart.Text = "bslchart";
+            this.BSLChart.Series.Add(series1);
+            this.BSLChart.Size = new System.Drawing.Size(1169, 648);
+            this.BSLChart.TabIndex = 0;
+            this.BSLChart.Text = "bslchart";
             title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             title1.Name = "Title1";
             title1.Position.Auto = false;
@@ -56,24 +57,26 @@ namespace GlucoCheck.Forms
             title1.Position.Width = 5F;
             title1.Text = "Blood Sugar Level";
             title1.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Rotated270;
-            this.bslchart.Titles.Add(title1);
-            this.bslchart.Click += new System.EventHandler(this.chart1_Click);
+            this.BSLChart.Titles.Add(title1);
+            this.BSLChart.Click += new System.EventHandler(this.chart1_Click);
             // 
             // FrmLogGraph
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.bslchart);
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.BSLChart);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmLogGraph";
             this.Text = "FrmLogGraph";
-            ((System.ComponentModel.ISupportInitialize)(this.bslchart)).EndInit();
+            this.Load += new System.EventHandler(this.FrmLogGraph_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BSLChart)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart bslchart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart BSLChart;
     }
 }
--- Forms/FrmLogGraph.cs
+++ Forms/FrmLogGraph.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GlucoCheck.Classes;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace GlucoCheck.Forms
 {
@@ -15,11 +17,31 @@ namespace GlucoCheck.Forms
         public FrmLogGraph()
         {
             InitializeComponent();
+
         }
-        private void Form1_Load(object sender, System.EventArgs e)
+        private void FrmLogGraph_Load(object sender, EventArgs e)
         {
-            //Adds some test elements to the graph
-            bslchart.Series["BSL"].Points.AddXY("Jan. 1", 180 + "mg/dl");
+            BSLChart.Series["BSL"].MarkerStyle = MarkerStyle.Circle;
+            BSLChart.Series["BSL"].XValueType = ChartValueType.DateTime;
+            BSLChart.ChartAreas["ChartArea1"].AxisX.IntervalType = DateTimeIntervalType.Auto;
+
+            // This can be changed to include the time with "hh:mm"
+            // however, when IntervalType = DateTimeIntervalType.Auto,
+            // the label tends to show "yyyy-MM-dd 12:00" each interval
+            BSLChart.ChartAreas["ChartArea1"].AxisX.LabelStyle.Format = "yyyy-MM-dd";
+
+            var fromDate = DateTime.Now.AddDays(-7);
+
+            using (var db = new AppDbContext())
+            {
+                foreach (var entry in db.Log.Where(l => l.EntryDate >= fromDate).OrderByDescending(l => l.Id)) {
+                    DataPoint point = new DataPoint();
+                    point.SetValueXY(entry.EntryDate, entry.BSL);
+                    point.ToolTip = entry.EasyDate + " at " + entry.EasyTime + ", " + entry.BSL + "mg/dl";
+                    BSLChart.Series["BSL"].Points.Add(point);
+                }
+            }
+
         }
 
         private void chart1_Click(object sender, EventArgs e)
