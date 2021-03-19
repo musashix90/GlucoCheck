1875e8f6f933a9a80a87ae4627f69f327972ecb3
Jacob Garner <jacobgarner185@outlook.com>
2021-03-19 15:18:57 -0400

    Added a date range filter to the log (still a WIP)

--- Forms/FrmLogViewer.Designer.cs
+++ Forms/FrmLogViewer.Designer.cs
@@ -29,23 +29,102 @@
         private void InitializeComponent()
         {
             this.DGVLogEntries = new System.Windows.Forms.DataGridView();
+            this.DTPFrom = new System.Windows.Forms.DateTimePicker();
+            this.Lbl2 = new System.Windows.Forms.Label();
+            this.Lbl3 = new System.Windows.Forms.Label();
+            this.DTPTo = new System.Windows.Forms.DateTimePicker();
+            this.SplContainerMain = new System.Windows.Forms.SplitContainer();
+            this.Lbl1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DGVLogEntries)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SplContainerMain)).BeginInit();
+            this.SplContainerMain.Panel1.SuspendLayout();
+            this.SplContainerMain.Panel2.SuspendLayout();
+            this.SplContainerMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // DGVLogEntries
             // 
             this.DGVLogEntries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVLogEntries.Location = new System.Drawing.Point(12, 12);
+            this.DGVLogEntries.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVLogEntries.Location = new System.Drawing.Point(0, 0);
             this.DGVLogEntries.Name = "DGVLogEntries";
-            this.DGVLogEntries.Size = new System.Drawing.Size(760, 437);
+            this.DGVLogEntries.Size = new System.Drawing.Size(517, 459);
             this.DGVLogEntries.TabIndex = 0;
+            // 
+            // DTPFrom
+            // 
+            this.DTPFrom.Location = new System.Drawing.Point(16, 67);
+            this.DTPFrom.Name = "DTPFrom";
+            this.DTPFrom.Size = new System.Drawing.Size(200, 20);
+            this.DTPFrom.TabIndex = 1;
+            this.DTPFrom.ValueChanged += new System.EventHandler(this.DTPFrom_ValueChanged);
+            // 
+            // Lbl2
+            // 
+            this.Lbl2.AutoSize = true;
+            this.Lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl2.Location = new System.Drawing.Point(13, 48);
+            this.Lbl2.Name = "Lbl2";
+            this.Lbl2.Size = new System.Drawing.Size(42, 16);
+            this.Lbl2.TabIndex = 3;
+            this.Lbl2.Text = "From:";
+            // 
+            // Lbl3
+            // 
+            this.Lbl3.AutoSize = true;
+            this.Lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl3.Location = new System.Drawing.Point(13, 97);
+            this.Lbl3.Name = "Lbl3";
+            this.Lbl3.Size = new System.Drawing.Size(28, 16);
+            this.Lbl3.TabIndex = 4;
+            this.Lbl3.Text = "To:";
+            // 
+            // DTPTo
+            // 
+            this.DTPTo.Location = new System.Drawing.Point(16, 116);
+            this.DTPTo.Name = "DTPTo";
+            this.DTPTo.Size = new System.Drawing.Size(200, 20);
+            this.DTPTo.TabIndex = 5;
+            this.DTPTo.ValueChanged += new System.EventHandler(this.DTPTo_ValueChanged);
+            // 
+            // SplContainerMain
+            // 
+            this.SplContainerMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SplContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplContainerMain.Location = new System.Drawing.Point(0, 0);
+            this.SplContainerMain.Name = "SplContainerMain";
+            // 
+            // SplContainerMain.Panel1
+            // 
+            this.SplContainerMain.Panel1.Controls.Add(this.Lbl1);
+            this.SplContainerMain.Panel1.Controls.Add(this.Lbl2);
+            this.SplContainerMain.Panel1.Controls.Add(this.DTPTo);
+            this.SplContainerMain.Panel1.Controls.Add(this.DTPFrom);
+            this.SplContainerMain.Panel1.Controls.Add(this.Lbl3);
+            // 
+            // SplContainerMain.Panel2
+            // 
+            this.SplContainerMain.Panel2.Controls.Add(this.DGVLogEntries);
+            this.SplContainerMain.Size = new System.Drawing.Size(784, 461);
+            this.SplContainerMain.SplitterDistance = 261;
+            this.SplContainerMain.TabIndex = 6;
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl1.Location = new System.Drawing.Point(12, 9);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(100, 20);
+            this.Lbl1.TabIndex = 6;
+            this.Lbl1.Text = "Filter options";
             // 
             // FrmLogViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.DGVLogEntries);
+            this.Controls.Add(this.SplContainerMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "FrmLogViewer";
@@ -55,6 +134,11 @@
             this.Text = "Log viewer";
             this.Load += new System.EventHandler(this.FrmLogViewer_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGVLogEntries)).EndInit();
+            this.SplContainerMain.Panel1.ResumeLayout(false);
+            this.SplContainerMain.Panel1.PerformLayout();
+            this.SplContainerMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SplContainerMain)).EndInit();
+            this.SplContainerMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -62,5 +146,11 @@
         #endregion
 
         private System.Windows.Forms.DataGridView DGVLogEntries;
+        private System.Windows.Forms.DateTimePicker DTPFrom;
+        private System.Windows.Forms.Label Lbl2;
+        private System.Windows.Forms.Label Lbl3;
+        private System.Windows.Forms.DateTimePicker DTPTo;
+        private System.Windows.Forms.SplitContainer SplContainerMain;
+        private System.Windows.Forms.Label Lbl1;
     }
 }
--- Forms/FrmLogViewer.cs
+++ Forms/FrmLogViewer.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Windows.Forms;
 using System.Linq;
 using GlucoCheck.Classes;
+using System.Windows.Forms;
 
 namespace GlucoCheck.Forms
 {
@@ -11,6 +11,20 @@ namespace GlucoCheck.Forms
         {
             InitializeComponent();
         }
+
+        #region Variables
+
+        /// <summary>
+        /// Indicates if the log should be filtered when a filter control's
+        /// value on the form is changed. You may want to disable this
+        /// when, say, the form is loading and you are setting the default
+        /// values of the controls.
+        /// </summary>
+        bool filterActive = true;
+
+        #endregion
+
+        #region Events
 
         private void FrmLogViewer_Load(object sender, EventArgs e)
         {
@@ -22,6 +36,9 @@ namespace GlucoCheck.Forms
                 DGVLogEntries.CellEndEdit += new DataGridViewCellEventHandler(DataGridView_CellEndEdit);
                 DGVLogEntries.CellValidating += new DataGridViewCellValidatingEventHandler(DataGridView_CellValidating);
             }
+
+            //Set form controls to their defualt values.
+            SetControlsToDefault();
         }
 
         private void DataGridView_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
@@ -37,5 +54,60 @@ namespace GlucoCheck.Forms
         {
             // TODO - add database updating mechanism
         }
+
+        private void DTPFrom_ValueChanged(object sender, EventArgs e)
+        {
+            FilterLogByDate();
+        }
+
+        private void DTPTo_ValueChanged(object sender, EventArgs e)
+        {
+            FilterLogByDate();
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Resets all the controls of the form to their default value.
+        /// </summary>
+        private void SetControlsToDefault()
+        {
+            //Disable the filter controls so that default values can be set.
+            filterActive = false;
+
+            //Set the default values for each control.
+            DTPFrom.Value = DateTime.Now;
+            DTPTo.Value = DateTime.Now.AddDays(-30);
+
+            //Enable the filter controls after setting their default values.
+            filterActive = true;
+        }
+
+        /// <summary>
+        /// Filters the log entries by the date range the user has selected.
+        /// </summary>
+        private void FilterLogByDate()
+        {
+            //Check to make sure the date range is valid.
+            if (DateTime.Compare(DTPFrom.Value, DTPTo.Value) < 0)
+            {
+                MessageBox.Show("The \"From\" date must be date that is " +
+                    "later than the \"To\" date!", "Invalid date range!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else if (filterActive)
+            {
+                //Filter the log entires by the from and to dates.
+                BindingSource source = new BindingSource();
+                source.DataSource = DGVLogEntries.DataSource;
+                source.Filter = "Data > " + DTPTo.Value + "AND Data < " + DTPFrom.Value;
+                DGVLogEntries.DataSource = source;
+            }
+        }
+
+        #endregion
     }
 }
