760116a19d524486fa4ca5d201259bd6cf8293da
Michael Tanner <mwtanner@my.waketech.edu>
2021-03-31 18:07:26 -0400

    Updated Reminders class for table name annotation, and created the initial migration for the Reminders table

--- Classes/Reminder.cs
+++ Classes/Reminder.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GlucoCheck.Classes
 {
+    [Table("reminders")]
     public class Reminder
     {
         #region Properties
---
+++ Migrations/202103312204398_InitialReminders.cs
@@ -0,0 +1,27 @@
+﻿namespace GlucoCheck.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialReminders : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.reminders",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        TaskToRemind = c.String(nullable: false, maxLength: 2147483647),
+                        TimeToRemind = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.reminders");
+        }
+    }
+}
