68af5b25531cd80cb46eb0653ca06ff096daadbc
Jacob Garner <jacobgarner185@outlook.com>
2021-04-16 16:42:12 -0400

    Update the forms so that none of them can be full screened (which would ruin the view). Also added a pop-up to let users know their settings have been saved.

--- Forms/FrmLogin.Designer.cs
+++ Forms/FrmLogin.Designer.cs
@@ -42,19 +42,19 @@ namespace GlucoCheck.Forms
             // 
             // UsernameTextbox
             // 
-            this.UsernameTextbox.Location = new System.Drawing.Point(470, 212);
-            this.UsernameTextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.UsernameTextbox.Location = new System.Drawing.Point(235, 110);
+            this.UsernameTextbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.UsernameTextbox.Name = "UsernameTextbox";
-            this.UsernameTextbox.Size = new System.Drawing.Size(237, 31);
+            this.UsernameTextbox.Size = new System.Drawing.Size(120, 20);
             this.UsernameTextbox.TabIndex = 0;
             this.UsernameTextbox.TextChanged += new System.EventHandler(this.UsernameTextbox_TextChanged);
             // 
             // PasswordTextbox
             // 
-            this.PasswordTextbox.Location = new System.Drawing.Point(470, 286);
-            this.PasswordTextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.PasswordTextbox.Location = new System.Drawing.Point(235, 149);
+            this.PasswordTextbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PasswordTextbox.Name = "PasswordTextbox";
-            this.PasswordTextbox.Size = new System.Drawing.Size(237, 31);
+            this.PasswordTextbox.Size = new System.Drawing.Size(120, 20);
             this.PasswordTextbox.TabIndex = 1;
             this.PasswordTextbox.UseSystemPasswordChar = true;
             this.PasswordTextbox.TextChanged += new System.EventHandler(this.PasswordTextbox_TextChanged);
@@ -62,29 +62,29 @@ namespace GlucoCheck.Forms
             // UsernameLabel
             // 
             this.UsernameLabel.AutoSize = true;
-            this.UsernameLabel.Location = new System.Drawing.Point(346, 214);
-            this.UsernameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.UsernameLabel.Location = new System.Drawing.Point(173, 111);
+            this.UsernameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.UsernameLabel.Name = "UsernameLabel";
-            this.UsernameLabel.Size = new System.Drawing.Size(116, 25);
+            this.UsernameLabel.Size = new System.Drawing.Size(58, 13);
             this.UsernameLabel.TabIndex = 2;
             this.UsernameLabel.Text = "Username:";
             // 
             // PasswordLabel
             // 
             this.PasswordLabel.AutoSize = true;
-            this.PasswordLabel.Location = new System.Drawing.Point(346, 288);
-            this.PasswordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PasswordLabel.Location = new System.Drawing.Point(173, 150);
+            this.PasswordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.PasswordLabel.Name = "PasswordLabel";
-            this.PasswordLabel.Size = new System.Drawing.Size(112, 25);
+            this.PasswordLabel.Size = new System.Drawing.Size(56, 13);
             this.PasswordLabel.TabIndex = 3;
             this.PasswordLabel.Text = "Password:";
             // 
             // LoginButton
             // 
-            this.LoginButton.Location = new System.Drawing.Point(315, 414);
-            this.LoginButton.Margin = new System.Windows.Forms.Padding(4);
+            this.LoginButton.Location = new System.Drawing.Point(158, 215);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(197, 60);
+            this.LoginButton.Size = new System.Drawing.Size(98, 31);
             this.LoginButton.TabIndex = 4;
             this.LoginButton.Text = "Login";
             this.LoginButton.UseVisualStyleBackColor = true;
@@ -92,10 +92,10 @@ namespace GlucoCheck.Forms
             // 
             // RegisterButton
             // 
-            this.RegisterButton.Location = new System.Drawing.Point(539, 414);
-            this.RegisterButton.Margin = new System.Windows.Forms.Padding(4);
+            this.RegisterButton.Location = new System.Drawing.Point(270, 215);
+            this.RegisterButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.RegisterButton.Name = "RegisterButton";
-            this.RegisterButton.Size = new System.Drawing.Size(199, 60);
+            this.RegisterButton.Size = new System.Drawing.Size(100, 31);
             this.RegisterButton.TabIndex = 5;
             this.RegisterButton.Text = "Register";
             this.RegisterButton.UseVisualStyleBackColor = true;
@@ -105,10 +105,10 @@ namespace GlucoCheck.Forms
             // 
             this.ErrorLabel.AutoSize = true;
             this.ErrorLabel.ForeColor = System.Drawing.Color.Red;
-            this.ErrorLabel.Location = new System.Drawing.Point(357, 339);
-            this.ErrorLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ErrorLabel.Location = new System.Drawing.Point(178, 176);
+            this.ErrorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ErrorLabel.Name = "ErrorLabel";
-            this.ErrorLabel.Size = new System.Drawing.Size(350, 25);
+            this.ErrorLabel.Size = new System.Drawing.Size(172, 13);
             this.ErrorLabel.TabIndex = 6;
             this.ErrorLabel.Text = "Username or password is incorrect.";
             this.ErrorLabel.Visible = false;
@@ -117,10 +117,10 @@ namespace GlucoCheck.Forms
             // 
             this.UserExistsErrorLabel.AutoSize = true;
             this.UserExistsErrorLabel.ForeColor = System.Drawing.Color.Red;
-            this.UserExistsErrorLabel.Location = new System.Drawing.Point(357, 365);
-            this.UserExistsErrorLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.UserExistsErrorLabel.Location = new System.Drawing.Point(178, 190);
+            this.UserExistsErrorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.UserExistsErrorLabel.Name = "UserExistsErrorLabel";
-            this.UserExistsErrorLabel.Size = new System.Drawing.Size(338, 25);
+            this.UserExistsErrorLabel.Size = new System.Drawing.Size(164, 13);
             this.UserExistsErrorLabel.TabIndex = 7;
             this.UserExistsErrorLabel.Text = "User already exists. Please log in.";
             this.UserExistsErrorLabel.Visible = false;
@@ -129,18 +129,18 @@ namespace GlucoCheck.Forms
             // 
             this.TitleLabel.AutoSize = true;
             this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.TitleLabel.Location = new System.Drawing.Point(328, 84);
-            this.TitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TitleLabel.Location = new System.Drawing.Point(164, 44);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TitleLabel.Name = "TitleLabel";
-            this.TitleLabel.Size = new System.Drawing.Size(397, 76);
+            this.TitleLabel.Size = new System.Drawing.Size(203, 39);
             this.TitleLabel.TabIndex = 8;
             this.TitleLabel.Text = "GlucoCheck";
             // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1053, 596);
+            this.ClientSize = new System.Drawing.Size(526, 310);
             this.Controls.Add(this.TitleLabel);
             this.Controls.Add(this.UserExistsErrorLabel);
             this.Controls.Add(this.ErrorLabel);
@@ -150,7 +150,8 @@ namespace GlucoCheck.Forms
             this.Controls.Add(this.UsernameLabel);
             this.Controls.Add(this.PasswordTextbox);
             this.Controls.Add(this.UsernameTextbox);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
             this.Name = "FrmLogin";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
